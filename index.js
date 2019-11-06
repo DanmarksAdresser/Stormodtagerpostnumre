95c2a2834545bce1bce9c6187319bc1c6cd5b4bd
finnjordal <finn@jordal.dk>
2019-11-06 16:44:13 +0100

    Søgning foretaget

--- index.js
+++ index.js
@@ -3,39 +3,53 @@
 const url = require('url');
 const rp = require('request-promise');
 const $ = require('cheerio');
-const kodelisterurl = 'https://www2.postdanmark.dk/findpostnummer/Findpostnummer.do';
+const puppeteer= require('puppeteer');
+const findpostnummerurl = 'http://www2.postdanmark.dk/findpostnummer/Findpostnummer.do';
+
 
 async function main() {
-	//debugger;
-	let html= await rp(kodelisterurl);
-	//console.log(html);
-	let kodelister= $('div > select > option', html);
-	//console.log('Antal kodelister: ' + kodelister.length);
-	//console.log(kodelister);
-	for (let i= 1; i<kodelister.length; i++) {
-		let kurl= url.parse(kodelister[i].attribs.value);
-		let names= kurl.pathname.split('/');
-		//console.log(names[names.length-1]);
-		startkode(names[names.length-1]);
-		let khtml= await rp(kodelister[i].attribs.value);		
-		let kodeliste= $('ul.kodeliste-list > li', khtml);
-		//console.log(kodeliste.length);
-		//let kodeognavn= /^([\d\w]+)\s-\s([\S+])$/;
-		let kodeognavn= /^([\d\w]+)\s-\s([\s\w\(\)æøåÆØÅé,.<>\-\/&§”:+…­­­­­­­½\[\]\–]+)$/;
-		for (let j= 0; j<kodeliste.length; j++) {
-			let result= kodeognavn.exec(kodeliste[j].children[0].data);
-			if (result) {
-				casekode(result[1], result[2]);
-				// console.log(result[1], + ': ' + result[2]);
-			}
-			else {				
-				console.log(kodeliste[j].children[0].data);
-			}
-		}
-		slutkode();
-	}
+	//debugger; input [name=]	
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(findpostnummerurl);
+  await page.click('input[name="zipcode"]');
+  await page.keyboard.type('3450');
+  await page.click('input[value="Søg"]');
 
+  await browser.close();
 }
+
+// async function main() {
+// 	//debugger;
+// 	let html= await rp(kodelisterurl);
+// 	//console.log(html);
+// 	let kodelister= $('td > input > option', html);
+// 	//console.log('Antal kodelister: ' + kodelister.length);
+// 	//console.log(kodelister);
+// 	for (let i= 1; i<kodelister.length; i++) {
+// 		let kurl= url.parse(kodelister[i].attribs.value);
+// 		let names= kurl.pathname.split('/');
+// 		//console.log(names[names.length-1]);
+// 		startkode(names[names.length-1]);
+// 		let khtml= await rp(kodelister[i].attribs.value);		
+// 		let kodeliste= $('ul.kodeliste-list > li', khtml);
+// 		//console.log(kodeliste.length);
+// 		//let kodeognavn= /^([\d\w]+)\s-\s([\S+])$/;
+// 		let kodeognavn= /^([\d\w]+)\s-\s([\s\w\(\)æøåÆØÅé,.<>\-\/&§”:+…­­­­­­­½\[\]\–]+)$/;
+// 		for (let j= 0; j<kodeliste.length; j++) {
+// 			let result= kodeognavn.exec(kodeliste[j].children[0].data);
+// 			if (result) {
+// 				casekode(result[1], result[2]);
+// 				// console.log(result[1], + ': ' + result[2]);
+// 			}
+// 			else {				
+// 				console.log(kodeliste[j].children[0].data);
+// 			}
+// 		}
+// 		slutkode();
+// 	}
+
+// }
 
 function startkode(kodelistenavn) {
 	console.log('');
